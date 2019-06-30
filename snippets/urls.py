165ad76def3e0d25d83e66c226e63368d70094d3
littlephoto <1605436974@qq.com>
2019-06-30 11:26:58 +0800

    更新viewset功能

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
-
+from rest_framework.schemas import get_schema_view
 from snippets import views
 
 # Create a router and register our viewset with it
@@ -11,8 +11,10 @@ router.register(r'users', views.UserViewSet)
 # The API URLs are now determined automatically by the router.
 # Additionally, we include the login URLs for the browsable API
 
+schema_view = get_schema_view(title='Pastebin API')
 
 urlpatterns = [
+    url(r'^schema/$', schema_view),
     url(r'^', include(router.urls)),
     url(r'api-auth', include('rest_framework.urls'))
 ]
